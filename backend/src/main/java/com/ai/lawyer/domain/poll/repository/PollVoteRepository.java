22f41c99a3084f35cfc5e262e7ff12508a765f6e
GarakChoi <tpghks312@naver.com>
2025-10-10 15:54:58 +0900

    feat[post]:myvotedpaged

--- backend/src/main/java/com/ai/lawyer/domain/poll/repository/PollVoteRepository.java
+++ backend/src/main/java/com/ai/lawyer/domain/poll/repository/PollVoteRepository.java
@@ -3,10 +3,12 @@ package com.ai.lawyer.domain.poll.repository;
 import com.ai.lawyer.domain.poll.entity.PollVote;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PollVoteRepository extends JpaRepository<PollVote, Long>, PollVoteRepositoryCustom {
     Optional<PollVote> findByMember_MemberIdAndPoll_PollId(Long memberId, Long pollId);
     void deleteByMember_MemberIdAndPoll_PollId(Long memberId, Long pollId);
     Optional<PollVote> findByMember_MemberIdAndPollOptions_PollItemsId(Long memberId, Long pollItemsId);
+    List<PollVote> findByMember_MemberId(Long memberId);
 }
